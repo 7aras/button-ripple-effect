ab00ea623e230ed9058f837bb66d9abb8329ca53
Taras Bielik <166549563+7aras@users.noreply.github.com>
2025-10-18 15:16:16 +0300

    added index js file changes

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const btnEl = document.querySelector(".btn"); 
 
-btnEl.addEventListener("mouseover", () => {
-    console.log(Event.pageY);
+btnEl.addEventListener("mouseover", (Event) => {
+    const x = Event.pageX - btnEl.offsetLeft;
+    const y = Event.pageY - btnEl.offsetTop;
+
+    btnEl.style.setProperty("--xPos", x + "px");
+    btnEl.style.setProperty("--yPos", y + "px");
         
 })
